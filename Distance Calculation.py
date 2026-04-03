8e00c87603f6342aed24d484da907580192467d3
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 12:03:38 +0530

    Create Distance Calculation.py

---
+++ Distance Calculation.py
@@ -0,0 +1,15 @@
+# SESSION 27 – Go-To-Goal (Distance Calculation)
+
+import math
+
+# Step 1: Input coordinates
+x1, y1 = 0, 0
+x2, y2 = 6, 8
+
+# Step 2: Compute distance
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+# Step 3: Display result
+print("Distance =", int(distance), "m")
+
+print("\nProgram Executed Successfully")
